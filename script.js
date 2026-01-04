38670450afcaf5c728c295c4db31ce3e0c33bbe9
Steve Cordeiro <escodsm@gmail.com>
2026-01-04 11:57:37 -0500

    Revert "Make title blue on click"
    This reverts commit 80f4c67a0bfdcb841b7d1f5ddd6687e099220aa4.

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const btn = document.getElementById("btn");
 const title = document.getElementById("title");
 
 btn.addEventListener("click", ()=> {
-	title.textContent = "Merged: red & blue";
-	title.style.color = "purple";
+	title.textContent = "Reverted: to black";
+	title.style.color = "black";
 });
