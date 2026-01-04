0262bf6c1f81a1c6b62a06670350e43448db103d
Steve Cordeiro <escodsm@gmail.com>
2026-01-04 12:56:58 -0500

    Log button click for debug

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const btn = document.getElementById("btn");
 const title = document.getElementById("title");
 
 btn.addEventListener("click", ()=> {
+    console.log("Button clicked...");
 	title.textContent = "Final attempt";
 	title.style.color = "pink";
 });
